45d30414b5bfb8cd42d79b8d372837b18907bff8
Davi Ribeiro <ribeirodavi04@outlook.com>
2021-08-01 20:25:51 -0300

    lista de tarefas adicionada

--- frontend/todo-app-react/src/components/Home/index.jsx
+++ frontend/todo-app-react/src/components/Home/index.jsx
@@ -3,14 +3,15 @@ import './style.css';
 import PageHeader from '../PageHeader';
 import { Container } from 'react-bootstrap';
 import TodoForm from '../TodoForm';
-import TodoList from '../../TodoList';
+import TodoList from '../TodoList';
 
 const Home = () => {
     return (
         <Container>
-            <PageHeader bigTitle="Pagina" smallTitle="inicial"/> 
+            <PageHeader bigTitle="Tarefas" smallTitle="Cadastro"/> 
             <TodoForm/>
-            <TodoList/>
+            <h2>Lista</h2>
+            <TodoList description="Tarefa zika"/>
         </Container>
     );
 };
