a7c7c5974ff701039a54ffcb13ba843f3342acf3
Davi Ribeiro <ribeirodavi04@outlook.com>
2021-08-02 22:08:30 -0300

    adicionado botão de pesquisa

--- frontend/todo-app-react/src/components/TodoForm/index.jsx
+++ frontend/todo-app-react/src/components/TodoForm/index.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { Button, Form,  Row, Col } from 'react-bootstrap';
-import { ImPlus } from "react-icons/im";
+import { ImPlus, ImSearch } from "react-icons/im";
 import { ContextApi } from '../../context';
 
 const TodoForm = () => {
@@ -11,11 +11,12 @@ const TodoForm = () => {
     return (
         <Form>
            <Form.Group as={Row} className="mb-3">
-               <Col sm={11}>
+               <Col md={10} xs={10} sm={8} className="col-6">
                    <Form.Control type="text" placeholder="Digite uma tarefa!" value={description} onChange={(e)=>setDescription(e.target.value)}/>
                </Col>
-               <Col sm={1}>
-                   <Button onClick={()=>addTask(description)}><ImPlus/></Button>
+               <Col md={2} xs={2} sm={4} col={6} className="col-6">
+                   <Button onClick={()=>addTask(description)} className="mr-3"><ImPlus/></Button>
+                   <Button ><ImSearch/></Button>
                </Col>
            </Form.Group>
             
